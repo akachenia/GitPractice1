d462ee7dddd7ec85e097077b3bc9834196568fbc
akachenia <akachenia@gmail.com>
2021-04-24 16:52:47 -0400

    added more work after publishing to github

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
     public static void main(String[] args){
 
         System.out.println("Hello world");
+        System.out.println("Adding GitHub now");
+        System.out.println("YAY STUFFS ARE ON GITHUB");
     }
 }
